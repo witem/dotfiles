68a9e275e28888ae3ece6cbe297516791ef18012
witem <1782530+witem@users.noreply.github.com>
2023-01-20 00:08:10 +0200

    master | new vim config
    Signed-off-by: witem <1782530+witem@users.noreply.github.com>

---
+++ nvim/lua/custom/mappings.lua
@@ -0,0 +1,9 @@
+local M = {}
+
+M.general = {
+  i = {
+    ["jk"] = { "<esc>", "exit from insert mode", opts = { noremap = true } },
+  },
+}
+
+return M
---
+++ nvim/lua/custom/plugins/null-ls.lua
@@ -0,0 +1,22 @@
+local present, null_ls = pcall(require, "null-ls")
+
+if not present then
+  return
+end
+
+local b = null_ls.builtins
+
+-- https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md
+local sources = {
+  -- webdev stuff
+  b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
+  b.formatting.prettier.with { filetypes = { "html", "markdown", "css" } }, -- so prettier works only on these filetypes
+
+  -- Lua
+  b.formatting.stylua,
+}
+
+null_ls.setup {
+  debug = true,
+  sources = sources,
+}
