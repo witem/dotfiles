f3b259a4ea88fa873f4078d9cac8ef7c860189c2
Artem Kuzmenko <artemk@defiyield.app>
2023-06-19 12:40:34 +0300

    feat(nvim): update nvchad

--- nvim/lua/custom/chadrc.lua
+++ nvim/lua/custom/chadrc.lua
@@ -1,14 +1,20 @@
--- First read our docs (completely) then check the example_config repo
-
+---@type ChadrcConfig
 local M = {}
 
+-- Path to overriding theme and highlights files
+local highlights = require "custom.highlights"
+
 M.ui = {
-  theme = "palenight",
+  theme = "onedark",
+  theme_toggle = { "onedark", "one_light" },
+
+  hl_override = highlights.override,
+  hl_add = highlights.add,
 }
 
+M.plugins = "custom.plugins"
 
-M.plugins = require "custom.plugins"
-
+-- check core.mappings for table structure
 M.mappings = require "custom.mappings"
 
 return M
--- nvim/lua/custom/configs/lspconfig.lua
+++ nvim/lua/custom/configs/lspconfig.lua
@@ -3,7 +3,8 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
 
-local servers = { "html", "cssls", "tsserver", "gopls" }
+-- if you just want default config for the servers then put them in a table
+local servers = { "html", "cssls", "tsserver", "clangd", "gopls" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
@@ -11,3 +12,6 @@ for _, lsp in ipairs(servers) do
     capabilities = capabilities,
   }
 end
+
+-- 
+-- lspconfig.pyright.setup { blabla}
